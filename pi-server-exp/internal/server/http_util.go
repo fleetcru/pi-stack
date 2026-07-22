00f3a7544847db255fe5206b391f823b71eba5e9
fleetcru <hello@fleetcru.dev>
2026-07-22 12:57:36 +0300

    fix: add PATCH to CORS allowed methods

--- pi-server-exp/internal/server/http_util.go
+++ pi-server-exp/internal/server/http_util.go
@@ -40,7 +40,7 @@ func corsMiddleware(allowed []string, next http.Handler) http.Handler {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
 		w.Header().Set("Vary", "Origin")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Request-ID")
 		w.Header().Set("Access-Control-Expose-Headers", "X-Request-ID")
 		w.Header().Set("Access-Control-Max-Age", "86400")
